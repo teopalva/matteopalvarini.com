2590c8de801c8296b2ff5ad8b4afc3890aa0f7b8
Matteo Palvarini <teo.palvarini@gmail.com>
2015-09-19 15:35:31 -0500

    Fixed hexagon animation

--- js/cell.js
+++ js/cell.js
@@ -4,18 +4,20 @@
     var imageWidth = 343;
     var imageHeight = 343;
     var radius = imageHeight / 2;
+    var duration = 1000;
 
     var cell = d3.select(".project");
 
     var canvas = cell.append("canvas")
         .attr("width", imageWidth)
-        .attr("height", imageHeight);
+        .attr("height", imageHeight)
+        .classed('canvas', true);
 
     var context = canvas.node().getContext("2d");
 
     var image = new Image;
     image.src = "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/0a8/0ee/14de48e.jpg";
-    //image.onload = resized;
+    //image.onload = func;
 
     function drawImage() {
         context.save();
@@ -23,22 +25,39 @@
         context.beginPath();
         context.moveTo(0, -radius);
         context.stroke();
+        context.lineWidth = 1;
+        context.strokeStyle = 'white';
         var currPos = [0, -radius];
 
-        var interpolate = function(currPos, B, steps, time) {
-            var unit = [B[0] / steps, B[1] / steps];
+        /**
+         * Performs linear interpolation between 2 points A and B.
+         * @param A coordinates of origin
+         * @param B coordinates of arrival point
+         * @param steps number of intermediate steps
+         * @param time total time
+         */
+        var interpolate = function(A, B, steps, time) {
+            var unit = [Math.abs(B[0] - A[0]) / steps, Math.abs(B[1] - A[1]) / steps];
+            var xSign = B[0] > A [0] ? +1 : -1;
+            var ySign = B[1] > A [1] ? +1 : -1;
             for(var i=0; i<steps; i++) {
-                (function(x, y) {
+                (function(x, y, i) {
                     var action = function() {
-                        context.lineTo(x, y);
+                        if(i === steps -1) {
+                            // Round the coords for last point
+                            context.lineTo(B[0], B[1]);
+                        } else {
+                            context.lineTo(x, y);
+                        }
                         context.stroke();
                         return true;
                     };
                     d3.timer(action, (time / steps) * i);
-                })(currPos[0] + unit[0] * (i+1), currPos[1] + unit[1] * (i+1));
+                })(A[0] + unit[0] * xSign * (i+1), A[1] + unit[1] * ySign * (i+1), i);
             }
         };
 
+        var durationEdge = duration / 6;
         for (var i = 1; i <= 7; i++) {
             var angle = i * Math.PI / 3,
                 x = Math.sin(angle) * radius,
@@ -48,29 +67,27 @@
                     if(i === 7) {
                         afterDrawing();
                     } else {
-                        context.lineTo(x, y);
-                        context.stroke();
-                        //interpolate(currPos, [x,y], 10, 1000);
+                        interpolate(currPos, [x,y], 10, durationEdge);
                     }
                     return true;
                 };
                 if(i === 1) {
                     action();
                 } else {
-                    d3.timer(action, 1000 * (i-1));
+                    d3.timer(action, durationEdge * (i-1));
                 }
             })(x, y, i, currPos);
             currPos = [x, y];
         }
 
         var afterDrawing = function() {
-            context.stroke();
             context.clip();
             context.drawImage(image,
                 0, 0,
                 imageWidth, imageHeight,
                 -imageWidth / 2, -imageHeight / 2,
                 imageWidth, imageHeight);
+            context.strokeStyle = null;
             context.restore();
         };
 
